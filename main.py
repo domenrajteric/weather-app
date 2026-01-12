c872fc6ebfb1f0418a577b95f955f13088489544
domenrajteric <95564636+domenrajteric@users.noreply.github.com>
2026-01-12 08:14:30 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -2,9 +2,12 @@
 from flask import Flask, render_template, request
 
 # Komunikacija/zahtevki med 2 aplikacijama
+# Knjižnico "requests" dodamo v "req.txt" oziroma jo namestimo s "pip install requests"
 import requests
 
 # Za dostop do okoljske spremenljivke (environment variable)
+# Namesto klasične spremenljivke, uporabimo okoljsko spremenljivko, ki je shranjena v sistemu
+# Na ta način zavarujemo svoj API ključ
 import os
 
 app = Flask(__name__)
@@ -19,6 +22,7 @@ def index():
         # API klic potrebuje spodnje 3 podatke oz. spremenljivke ...
         city = request.form.get("city")         # Kraj
         unit = "metric"                         # Merska enota: "metric" ali "imperial"
+        #api_key = "tvoj API ključ"              # Samo za testno okolje, kasneje zamenjaj z okoljsko spremenljivko (glej spodaj)
         api_key = os.environ.get("API_KEY")     # API ključ, ki mora ostati "skrit"
 
         # URL za klic/zahtevek, ki vključuje zgornje 3 informacije
